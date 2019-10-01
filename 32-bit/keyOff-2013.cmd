d60c84f9265ea47265948f56d216863b38824430
gatoslu <lbcfez@gmail.com>
2019-10-01 22:56:22 +0800

    Add files via upload
    add keyOff-2013 file

---
+++ 32-bit/keyOff-2013.cmd
@@ -0,0 +1,71 @@
+if "%~1"=="" exit /b
+goto :%1 2>nul || exit /b
+
+:: Office 2013
+:dc981c6b-fc8e-420f-aa43-f8f33e5c0923
+set "key=42QTK-RN8M7-J3C4G-BBGYM-88CYV" &:: Mondo
+exit /b
+
+:b322da9c-a2e2-4058-9e4e-f59a6970bd69
+set "key=YC7DK-G2NP3-2QQC3-J6H88-GVGXT" &:: Professional Plus
+exit /b
+
+:b13afb38-cd79-4ae5-9f7f-eed058d750ca
+set "key=KBKQT-2NMXY-JJWGP-M62JB-92CD4" &:: Standard
+exit /b
+
+:4a5d124a-e620-44ba-b6ff-658961b33b9a
+set "key=FN8TT-7WMH6-2D4X9-M337T-2342K" &:: Project Professional
+exit /b
+
+:427a28d1-d17c-4abf-b717-32c780ba6f07
+set "key=6NTH3-CW976-3G3Y2-JK3TX-8QHTT" &:: Project Standard
+exit /b
+
+:e13ac10e-75d0-4aff-a0cd-764982cf541c
+set "key=C2FG9-N6J68-H8BTJ-BW3QX-RM3B3" &:: Visio Professional
+exit /b
+
+:ac4efaf0-f81f-4f61-bdf7-ea32b02ab117
+set "key=J484Y-4NKBF-W2HMG-DBMJC-PGWR7" &:: Visio Standard
+exit /b
+
+:6ee7622c-18d8-4005-9fb7-92db644a279b
+set "key=NG2JY-H4JBT-HQXYP-78QH9-4JM2D" &:: Access
+exit /b
+
+:f7461d52-7c2b-43b2-8744-ea958e0bd09a
+set "key=VGPNG-Y7HQW-9RHP7-TKPV3-BG7GB" &:: Excel
+exit /b
+
+:fb4875ec-0c6b-450f-b82b-ab57d8d1677f
+set "key=H7R7V-WPNXQ-WCYYC-76BGV-VT7GH" &:: Groove
+exit /b
+
+:a30b8040-d68a-423f-b0b5-9ce292ea5a8f
+set "key=DKT8B-N7VXH-D963P-Q4PHY-F8894" &:: InfoPath
+exit /b
+
+:1b9f11e3-c85c-4e1b-bb29-879ad2c909e3
+set "key=2MG3G-3BNTT-3MFW9-KDQW3-TCK7R" &:: Lync
+exit /b
+
+:efe1f3e6-aea2-4144-a208-32aa872b6545
+set "key=TGN6P-8MMBC-37P2F-XHXXK-P34VW" &:: OneNote
+exit /b
+
+:771c3afa-50c5-443f-b151-ff2546d863a0
+set "key=QPN8Q-BJBTJ-334K3-93TGY-2PMBT" &:: Outlook
+exit /b
+
+:8c762649-97d1-4953-ad27-b7e2c25b972e
+set "key=4NT99-8RJFH-Q2VDH-KYG2C-4RD4F" &:: Powerpoint
+exit /b
+
+:00c79ff1-6850-443d-bf61-71cde0de305f
+set "key=PN2WF-29XG2-T9HJ7-JQPJR-FCXK4" &:: Publisher
+exit /b
+
+:d9f5b1c6-5386-495a-88f9-9ad6b41ac9b3
+set "key=6Q7VD-NX8JD-WJ2VH-88V73-4GBJ7" &:: Word
+exit /b
